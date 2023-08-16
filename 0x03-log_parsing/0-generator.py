77ba1fa891e06dbe7ee3a7522e1bb2185b977165
kenworw <tkelemu165@gmail.com>
2023-08-16 16:53:46 +0300

    Log parsing

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 import random
 import sys
 from time import sleep
@@ -13,4 +12,4 @@ for i in range(10000):
         random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
         random.randint(1, 1024)
     ))
-    sys.stdout.flush()
+    sys.stdout.flush() 
