62de0acbd64e4f91b7f5c21c2164dc7e47d87072
kenworw <tkelemu165@gmail.com>
2023-08-18 00:04:03 +0300

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,70 +1,37 @@
 #!/usr/bin/python3
-"""
-Log parsing - a script that reads stdin line by line and computes metrics:
-    Input format:   <IP Address> - [<date>]
-                    "GET /projects/260 HTTP/1.1" <status code> <file size>
-    (if the format is not this one, the line must be skipped)
-    After every 10 lines and/or a keyboard interruption (CTRL + C)
-    print these statistics from the beginning:
-        Number of lines by status code: possible status code:
-            200, 301, 400, 401, 403, 404, 405 and 500
-        if a status code doesn’t appear or is not an integer,
-            don’t print anything for this status code
-        format: <status code>: <number>
-        status codes should be printed in ascending order
-"""
-import re
+'''A script that reads stdin line by line and computes metrics'''
+
+
 import sys
-import signal
 
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
 
-def print_statistics(size, statusCount):
-    """
-    Prints logged statistics
-    """
-    print("File size: {:d}".format(size))
-    for key, value in sorted(statusCount.items()):
-        print("{}: {:d}".format(key, value))
-
-
-def main():
-    """
-    Reads stdin
-    """
-    count: int = 0
-    size: int = 0
-    statusCount = {}
-    pattern = r'^(\d+\.\d+\.\d+\.\d+) - \[(.*?)\] "([^"]+)" (\d+) (\d+)$'
-
-    def handle_interrupt(signum, frame):
-        print_statistics(size, statusCount)
-        sys.exit(0)
-
-    signal.signal(signal.SIGINT, handle_interrupt)
-
+try:
     for line in sys.stdin:
-        line = line.strip()
-        if line == "exit":
-            break
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
 
-        match = re.match(pattern, line)
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
-        if match:
-            count += 1
-            if str(int(match.group(5))) == match.group(5):
-                size = size + int(match.group(5))
-            if str(int(match.group(4))) == match.group(4):
-                if str(match.group(4)) in statusCount:
-                    statusCount[str(match.group(4))] = statusCount[
-                            str(match.group(4))] + 1
-                else:
-                    statusCount[str(match.group(4))] = 1
+except Exception as err:
+    pass
 
-        if count % 10 == 0:
-            print_statistics(size, statusCount)
-
-    print_statistics(size, statusCount)
-
-
-if __name__ == "__main__":
-    main()
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
